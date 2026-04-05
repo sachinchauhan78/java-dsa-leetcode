20b64e729dc31d4cd49986ecb94f0926e4674687
sachinchauhan78 <darkknight@gmail.com>
2026-04-05 17:14:29 +0530

    arrayinputoutput

---
+++ Arrays/ArrayOutputIntput.java
@@ -0,0 +1,12 @@
+package com.javabasics.Arrays;
+
+public class ArrayOutputIntput {
+    public static void main(String[] args){
+        int[] arr={2,4,6,8,90};
+        System.out.println(arr);
+        for(int i=0;i<=4;i++){
+            System.out.println(arr[i]+" ");
+        }
+
+    }
+}
