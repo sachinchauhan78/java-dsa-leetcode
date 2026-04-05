3c46125f50efae655b3728d50d384aa2684c404b
sachinchauhan78 <darkknight@gmail.com>
2026-04-05 17:17:13 +0530

    hh

--- array1.java
+++
@@ -1,8 +0,0 @@
-package com.javabasics;
-
-public class array1 {
-    public static void main(strings... args){
-        int[] x={2,4,67,89,0};
-        System.out.println(x[3]);
-    }
-}
---
+++ strings.java
@@ -0,0 +1,4 @@
+package com.javabasics;
+
+public class strings {
+}
